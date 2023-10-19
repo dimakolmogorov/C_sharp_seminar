a4878a9375da5cb7e6fde99e082c95c44e3adaa1
Dmitriy <dima.kolmogorov.1993@gmail.com>
2023-10-19 19:12:58 +0300

    initial commit

---
+++ zadacha25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите степень");
+        int b = Convert.ToInt32(Console.ReadLine());
+        double res = (Math.Pow(a, b));
+         Console.WriteLine(res);
+    }
+}
---
+++ zadacha29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arrayRandom = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    arrayRandom[i] = new Random().Next(1, 999);
+    System.Console.WriteLine(arrayRandom[i] + " ");
+
+}
